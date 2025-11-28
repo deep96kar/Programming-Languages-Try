b023be2b1ef6b23a77e3630caeeaf00316780dd2
deep96kar <deep96krk.in>
2025-11-28 21:10:51 +0530

    Complete

---
+++ Javascript try/IF & ELSE.js
@@ -0,0 +1,14 @@
+let a = 3;
+let b = 4;
+
+if (a === b) {
+  console.log("equal");
+} else {
+  console.log("not equal");
+}
+
+if (!(a ^ b)) {
+  console.log("equal");
+} else {
+  console.log("not equal");
+}
